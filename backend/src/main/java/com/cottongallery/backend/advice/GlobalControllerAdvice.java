f7f585e568ff5b2781e7cb72c68789566f13824e
iju42829 <ijud4282@gmail.com>
2024-10-03 16:42:41 +0900

    feat: 유효성 검증 오류에 대한 처리 프로세스 구현

---
+++ backend/src/main/java/com/cottongallery/backend/advice/GlobalControllerAdvice.java
@@ -0,0 +1,34 @@
+package com.cottongallery.backend.advice;
+
+import com.cottongallery.backend.dto.Response;
+import com.cottongallery.backend.exception.InvalidRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<Response> handleInvalidRequestException(InvalidRequestException e) {
+        List<Map<String, String>> errors = e.getBindingResult().getFieldErrors().stream().map(fieldError -> {
+            Map<String, String> errorMap = new HashMap<>();
+
+            errorMap.put("field", fieldError.getField());
+            errorMap.put("code", fieldError.getCode());
+            errorMap.put("message", fieldError.getDefaultMessage());
+
+            return errorMap;
+
+        }).toList();
+
+        return new ResponseEntity<>(new Response(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), e.getMessage(), errors),
+                HttpStatus.BAD_REQUEST);
+    }
+}
--- backend/src/main/java/com/cottongallery/backend/controller/AccountController.java
+++ backend/src/main/java/com/cottongallery/backend/controller/AccountController.java
@@ -4,10 +4,12 @@ import com.cottongallery.backend.controller.validator.AccountCreateRequestValida
 import com.cottongallery.backend.dto.Response;
 import com.cottongallery.backend.dto.account.request.AccountCreateRequest;
 import com.cottongallery.backend.dto.account.response.AccountCheckUsernameResponse;
+import com.cottongallery.backend.exception.InvalidRequestException;
 import com.cottongallery.backend.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +32,11 @@ public class AccountController {
     }
 
     @PostMapping(value = "/sign-up", consumes = APPLICATION_JSON_VALUE)
-    public ResponseEntity<Response> addAccount(@Validated @RequestBody AccountCreateRequest accountCreateRequest) {
+    public ResponseEntity<Response> addAccount(@Validated @RequestBody AccountCreateRequest accountCreateRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestException("회원 가입 요청 값이 올바르지 않습니다. 다시 확인해 주세요.", bindingResult);
+        }
+
         accountService.signUp(accountCreateRequest);
 
         return new ResponseEntity<>(new Response(LocalDateTime.now(), HttpStatus.CREATED.value(), "회원 가입 성공", null),
