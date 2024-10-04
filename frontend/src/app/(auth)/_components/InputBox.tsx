98688472eb3b7a3bab60950aa269444942bb6170
oneieo <sun020716@gmail.com>
2024-10-04 21:26:29 +0900

    script: before create branch

---
+++ frontend/src/app/(auth)/_components/InputBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputBox = () => {
+  return <div>인풋박스 컴포넌트 - 라벨, 인풋</div>;
+};
+
+export default InputBox;
--- frontend/src/app/(auth)/join/page.tsx
+++ frontend/src/app/(auth)/join/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const page = () => {
-  return <div>회원가입 페이지</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1>회원가입 페이지</h1>
+      <form>{/*  */}</form>
+    </div>
+  );
 };
 
 export default page;
