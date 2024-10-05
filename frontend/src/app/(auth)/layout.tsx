d07341792dd1ebff63d57e90b757b80ee60b8c26
oneieo <sun020716@gmail.com>
2024-10-05 21:55:49 +0900

    add: logo component

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,9 +1,10 @@
 import { PropsWithChildren } from "react";
+import AuthHeader from "./_components/AuthHeader";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="w-full h-screen flex- flex-col overflow-y-auto scrollbar-hide scroll-smooth">
-      {/* <AuthHeader/> */}
+      <AuthHeader />
       <div className="flex flex-col flex-grow relative">{children}</div>
     </div>
   );
