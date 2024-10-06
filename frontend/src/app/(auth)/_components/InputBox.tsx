54434d0bdb53f745f3fc2225999938de46d2e535
oneieo <sun020716@gmail.com>
2024-10-06 23:53:07 +0900

    chore: create InputBox component

--- frontend/src/app/(auth)/_components/InputBox.tsx
+++ frontend/src/app/(auth)/_components/InputBox.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const InputBox = () => {
-  return <div>인풋박스 컴포넌트 - 라벨, 인풋</div>;
+type PropsType = {
+  id: "username" | "password" | "passwordConfirm" | "name" | "phoneNumber";
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  text: string;
+  error: Record<string, string>;
+  //ishide?: boolean;
+  //setHide?: Dispatch<SetStateAction<boolean>>;
+};
+
+const InputBox = ({ id, type, placeholder, value, onChange, text, error }: PropsType) => {
+  // TODO: 비밀번호 눈 아이콘 로직 추가
+  return (
+    <div>
+      <label htmlFor="id">{text}</label>
+      <input id={id} type={type} value={value} onChange={onChange} placeholder={placeholder} />
+      {/* 아이콘 위치 */}
+      <p>{error[id]}</p>
+    </div>
+  );
 };
 
 export default InputBox;
