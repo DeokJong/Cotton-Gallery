127509a241accb8875cc945e4aaa68b4022dbd65
oneieo <sun020716@gmail.com>
2024-10-28 00:19:23 +0900

    update: 로그인 기능

--- frontend/src/app/(auth)/_components/Login.tsx
+++ frontend/src/app/(auth)/_components/Login.tsx
@@ -3,8 +3,12 @@
 import React from "react";
 import InputBox from "./InputBox";
 import { useAuthStore } from "@/store/authStore";
+import SubmitBtn from "./SubmitBtn";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+  const router = useRouter();
   const { username, password, error, setUsername, setPassword, setError } = useAuthStore();
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,22 +20,25 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  const handleSubmitSignUpForm = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmitLoginForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const response = await fetch("/api/login", {
+    const response = await fetch("http://localhost:8080/api/login", {
       method: "POST",
+      credentials: "include",
       headers: {
-        "Content-Type": "application/json" // 헤더 추가
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         username,
         password
       })
     });
-
     const result = await response.json();
     console.log(result);
+    if (result.status === 200) {
+      router.push("/");
+    }
     //console.log(typeof phoneNumber);
     //console.log(name, username, password, phoneNumber, passwordConfirm, email, zipcode, street, detail);
   };
@@ -39,7 +46,7 @@ const Login = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-[1.75rem] mb-[3.438rem]">로그인</h1>
-      <form onSubmit={handleSubmitSignUpForm} className="flex flex-col gap-[1.875rem]">
+      <form onSubmit={handleSubmitLoginForm} className="flex flex-col gap-[1.875rem]">
         <InputBox
           id="username"
           text="아이디"
@@ -58,6 +65,13 @@ const Login = () => {
           onChange={handlePasswordChange}
           error={error}
         />
+        <div className="flex justify-between ml-2 mr-2 text-gray-500 ">
+          <p>아직 회원이 아니신가요?</p>
+          <Link href={"/join"}>
+            <p className="text-gray-500 font-semibold">회원가입</p>
+          </Link>
+        </div>
+        <SubmitBtn text="login" />
       </form>
     </div>
   );
---
+++ frontend/src/app/api/login/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const requestData = await request.json();
+    const response = await fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(requestData)
+    });
+
+    const data = await response.json();
+
+    return new NextResponse(JSON.stringify(data), {
+      status: response.status,
+      headers: { "Content-Type": "application/json" }
+    });
+  } catch (error) {
+    console.error("error", error);
+    return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+}
