0038dd274434bc249e1c15ffde2643b5eff87bf5
iju42829 <ijud4282@gmail.com>
2024-11-06 23:07:46 +0900

    feat: 할인 컨트롤러 구현

---
+++ backend/src/main/java/com/cottongallery/backend/item/controller/DiscountController.java
@@ -0,0 +1,63 @@
+package com.cottongallery.backend.item.controller;
+
+import com.cottongallery.backend.common.dto.DataResponse;
+import com.cottongallery.backend.common.dto.PageInfo;
+import com.cottongallery.backend.common.dto.Response;
+import com.cottongallery.backend.item.dto.request.DiscountCreateRequest;
+import com.cottongallery.backend.item.dto.request.DiscountUpdateRequest;
+import com.cottongallery.backend.item.dto.response.DiscountListResponse;
+import com.cottongallery.backend.item.dto.response.DiscountResponse;
+import com.cottongallery.backend.item.service.DiscountService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/discounts")
+@RequiredArgsConstructor
+public class DiscountController {
+
+    private final DiscountService discountService;
+
+    @PostMapping
+    public ResponseEntity<Response> addDiscount(@RequestBody DiscountCreateRequest discountCreateRequest) {
+        discountService.createDiscount(discountCreateRequest);
+
+        return new ResponseEntity<>(new Response(HttpServletResponse.SC_OK, "할인 생성에 성공했습니다."), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<DataResponse> retrieveDiscounts(@RequestParam(defaultValue = "1") int page) {
+        PageRequest pageRequest = PageRequest.of(page - 1, 10, Sort.by(Sort.Direction.DESC, "createdBy"));
+
+        Slice<DiscountResponse> discounts = discountService.getDiscounts(pageRequest);
+
+        List<DiscountResponse> content = discounts.getContent();
+        PageInfo pageInfo = new PageInfo(page, discounts.hasNext(), discounts.hasPrevious());
+
+        DiscountListResponse discountListResponse = new DiscountListResponse(pageInfo, content);
+
+        return new ResponseEntity<>(new DataResponse(HttpServletResponse.SC_OK, "할인 생성에 성공했습니다.", discountListResponse), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{discountId}")
+    public ResponseEntity<Response> editDiscount(@PathVariable Long discountId, @RequestBody DiscountUpdateRequest discountUpdateRequest) {
+        discountService.updateDiscount(discountId, discountUpdateRequest);
+
+        return new ResponseEntity<>(new Response(HttpServletResponse.SC_OK, "할인 " + discountId + " 수정에 성공했습니다."), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{discountId}")
+    public ResponseEntity<Response> removeDiscount(@PathVariable Long discountId) {
+        discountService.deleteDiscount(discountId);
+
+        return new ResponseEntity<>(new Response(HttpServletResponse.SC_OK, "할인 " + discountId + " 삭제에 성공했습니다."), HttpStatus.OK);
+    }
+}
