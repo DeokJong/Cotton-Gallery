1bcaa67f632f8121e11ec7e3d1685a94701f89d2
oneieo <sun020716@gmail.com>
2024-11-07 23:47:55 +0900

    chore: 메인 헤더 수정

--- frontend/src/app/(main)/_components/MainHeader.tsx
+++ frontend/src/app/(main)/_components/MainHeader.tsx
@@ -3,8 +3,14 @@ import React from "react";
 
 const MainHeader = () => {
   return (
-    <div className="w-full mt-[3.75rem] mb-[3.75rem] flex justify-center">
+    <div className="w-full mt-[3.75rem] mb-[3.75rem] flex justify-center gap-[22.5rem]">
+      <p>찜 리스트</p>
       <Logo />
+      {/* TODO: 로그인 여부 따라 다르게 렌더링 */}
+      <div className="flex gap-3">
+        <button>OOO님</button>
+        <button>로그아웃</button>
+      </div>
     </div>
   );
 };
