a0685144e50e7765ea4111d4689589632c4e9225
oneieo <sun020716@gmail.com>
2024-11-14 23:24:05 +0900

    update: 로그인 한 유저 이름 렌더링

--- frontend/src/app/(auth)/_components/Login.tsx
+++ frontend/src/app/(auth)/_components/Login.tsx
@@ -9,7 +9,7 @@ import { useRouter } from "next/navigation";
 
 const Login = () => {
   const router = useRouter();
-  const { username, password, error, setUsername, setPassword, setError } = useAuthStore();
+  const { username, password, error, setName, setUsername, setPassword, setError } = useAuthStore();
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
@@ -33,7 +33,7 @@ const Login = () => {
       return;
     }
 
-    const response = await fetch("http://localhost:8080/api/login", {
+    const response = await fetch("http://localhost:8080/api/auth/login", {
       method: "POST",
       credentials: "include",
       headers: {
@@ -47,6 +47,9 @@ const Login = () => {
     const result = await response.json();
     console.log(result);
     if (result.status === 200) {
+      if (result.data.name) {
+        setName(result.data.name);
+      }
       router.push("/");
     }
     //console.log(typeof phoneNumber);
--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -1,42 +1,55 @@
 import { AuthStoreType } from "@/types/auth.type";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useAuthStore = create<AuthStoreType>((set) => ({
-  username: "",
-  password: "",
-  passwordConfirm: "",
-  name: "",
-  phoneNumber: "",
-  email: "",
-  zipcode: "",
-  street: "",
-  detail: "",
-  error: {
-    username: "",
-    password: "",
-    passwordConfirm: "",
-    name: "",
-    phoneNumber: "",
-    email: "",
-    zipcode: "",
-    street: "",
-    detail: ""
-  },
-
-  setUsername: (username: string) => set({ username }),
-  setPassword: (password: string) => set({ password }),
-  setPasswordConfirm: (passwordConfirm: string) => set({ passwordConfirm }),
-  setName: (name: string) => set({ name }),
-  setPhoneNumber: (phoneNumber: string) => set({ phoneNumber }),
-  setEmail: (email: string) => set({ email }),
-  setZipcode: (zipcode: string) => set({ zipcode }),
-  setStreet: (street: string) => set({ street }),
-  setDetail: (detail: string) => set({ detail }),
-  setError: (field: string, message: string) =>
-    set((state) => ({
+export const useAuthStore = create(
+  persist<AuthStoreType>(
+    (set) => ({
+      username: "",
+      password: "",
+      passwordConfirm: "",
+      name: "",
+      phoneNumber: "",
+      email: "",
+      zipcode: "",
+      street: "",
+      detail: "",
       error: {
-        ...state.error,
-        [field]: message
-      }
-    }))
-}));
+        username: "",
+        password: "",
+        passwordConfirm: "",
+        name: "",
+        phoneNumber: "",
+        email: "",
+        zipcode: "",
+        street: "",
+        detail: ""
+      },
+
+      setUsername: (username: string) => set({ username }),
+      setPassword: (password: string) => set({ password }),
+      setPasswordConfirm: (passwordConfirm: string) => set({ passwordConfirm }),
+      setName: (name: string) => set({ name }),
+      setPhoneNumber: (phoneNumber: string) => set({ phoneNumber }),
+      setEmail: (email: string) => set({ email }),
+      setZipcode: (zipcode: string) => set({ zipcode }),
+      setStreet: (street: string) => set({ street }),
+      setDetail: (detail: string) => set({ detail }),
+      setError: (field: string, message: string) =>
+        set((state) => ({
+          error: {
+            ...state.error,
+            [field]: message
+          }
+        }))
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) =>
+        ({
+          name: state.name
+        }) as AuthStoreType
+    }
+  )
+);
