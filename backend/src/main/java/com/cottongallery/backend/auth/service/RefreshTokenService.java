65b62aa96c76b60cbc1dd1ef830d9d95c30a3da9
DeokJong <novelss444@jbnu.ac.kr>
2024-11-15 17:43:05 +0900

    feature: Refresh Token With Redis Database

--- backend/src/main/java/com/cottongallery/backend/auth/service/RefreshTokenService.java
+++ backend/src/main/java/com/cottongallery/backend/auth/service/RefreshTokenService.java
@@ -1,25 +1,39 @@
 package com.cottongallery.backend.auth.service;
 
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ConcurrentHashMap;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class RefreshTokenService {
-    // TODO : DB 사용 
-    private final ConcurrentHashMap<String, String> refreshTokenStore = new ConcurrentHashMap<>();
+    private final RedisTemplate<String, String> redisTemplate;
+    @Value("${jwt.authorization-header-refresh}")
+    private String AUTHORIZATION_HEADER_REFRESH;
+    @Value("${jwt.refresh-token-validity-in-seconds}")
+    private long REFRESH_TOKEN_EXPIRATION;
 
     public void saveRefreshToken(String username, String refreshToken) {
-        refreshTokenStore.put(username, refreshToken);
+      String key = getKey(username, refreshToken);
+      redisTemplate.opsForValue().set(key, "", Duration.ofSeconds(REFRESH_TOKEN_EXPIRATION));
     }
 
-    public boolean isValidRefreshToken(String username, String refreshToken) {
-        String storedToken = refreshTokenStore.get(username);
-        return storedToken != null && storedToken.equals(refreshToken);
+    public boolean validateRefreshToken(String username, String refreshToken) {
+      String key = getKey(username, refreshToken);
+      return Boolean.TRUE.equals(redisTemplate.hasKey(key));
     }
 
-    public void deleteRefreshToken(String username) {
-        refreshTokenStore.remove(username);
+    public void deleteRefreshToken(String username, String refreshToken) {
+      String key = getKey(username, refreshToken);
+      redisTemplate.delete(key);
     }
+
+    private String getKey(String username, String refreshToken) {
+      return AUTHORIZATION_HEADER_REFRESH + username + ":" + refreshToken;
+  }
 }
 
