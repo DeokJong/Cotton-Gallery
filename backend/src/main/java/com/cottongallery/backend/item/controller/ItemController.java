7a65fb2068e440dac155307d91157756cca60f38
iju42829 <ijud4282@gmail.com>
2024-11-15 13:35:42 +0900

    refactor: ItemController Swagger 인터페이스로 분리

--- backend/src/main/java/com/cottongallery/backend/item/controller/ItemController.java
+++ backend/src/main/java/com/cottongallery/backend/item/controller/ItemController.java
@@ -4,15 +4,11 @@ import com.cottongallery.backend.common.dto.ListResponse;
 import com.cottongallery.backend.common.dto.PageInfo;
 import com.cottongallery.backend.common.dto.Response;
 import com.cottongallery.backend.common.exception.InvalidRequestException;
+import com.cottongallery.backend.item.controller.api.ItemApi;
 import com.cottongallery.backend.item.dto.request.ItemCreateRequest;
 import com.cottongallery.backend.item.dto.request.ItemUpdateRequest;
 import com.cottongallery.backend.item.dto.response.ItemListResponse;
 import com.cottongallery.backend.item.service.ItemService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,18 +28,12 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/items")
-@Tag(name = "상품 관리", description = "상품 관련 API")
-public class ItemController {
+public class ItemController implements ItemApi {
 
     private final ItemService itemService;
 
-    @Operation(summary = "상품 등록", description = "새로운 상품을 등록합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "상품 등록 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 상품 등록 요청")
-    })
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Response<?>> addItem(@Parameter(description = "적용할 할인 ID. 할인 미적용 시 생략 가능") @RequestParam(required = false) Long discountId,
+    public ResponseEntity<Response<?>> addItem(@RequestParam(required = false) Long discountId,
                                                @Validated @RequestBody ItemCreateRequest itemCreateRequest,
                                                BindingResult bindingResult) {
 
@@ -58,12 +48,8 @@ public class ItemController {
         return new ResponseEntity<>(Response.createResponseWithoutData(HttpServletResponse.SC_CREATED, "상품 생성에 성공했습니다."), HttpStatus.CREATED);
     }
 
-    @Operation(summary = "상품 목록 조회", description = "특정 페이지의 상품 목록을 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "특정 페이지 상품 목록 조회 성공")
-    })
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Response<ListResponse<List<ItemListResponse>>>> retrieveItems(@Parameter(description = "조회할 페이지 번호") @RequestParam(defaultValue = "1") int page) {
+    public ResponseEntity<Response<ListResponse<List<ItemListResponse>>>> retrieveItems(@RequestParam(defaultValue = "1") int page) {
         PageRequest pageRequest = PageRequest.of(page -1, 10, Sort.by(Sort.Direction.DESC, "createdBy"));
 
         Slice<ItemListResponse> items = itemService.getItemResponses(pageRequest);
@@ -74,14 +60,9 @@ public class ItemController {
         return new ResponseEntity<>(Response.createResponse(HttpServletResponse.SC_OK, "상품 " + page + " 페이지 조회에 성공했습니다.", itemResponse), HttpStatus.OK);
     }
 
-    @Operation(summary = "상품 수정", description = "특정 상품을 수정합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "상품 수정 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 상품 수정 요청")
-    })
     @PatchMapping(value = "/{itemId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Response<?>> editItem(@Parameter(description = "수정할 상품 ID") @PathVariable Long itemId,
-                                                @Parameter(description = "변경할 할인 ID, 할인 미적용 시 생략 가능") @RequestParam(required = false) Long discountId,
+    public ResponseEntity<Response<?>> editItem(@PathVariable Long itemId,
+                                                @RequestParam(required = false) Long discountId,
                                                 @Validated @RequestBody ItemUpdateRequest itemUpdateRequest,
                                                 BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -95,12 +76,8 @@ public class ItemController {
         return new ResponseEntity<>(Response.createResponseWithoutData(HttpServletResponse.SC_OK, "상품 수정에 성공했습니다."), HttpStatus.OK);
     }
 
-    @Operation(summary = "상품 삭제", description = "특정 상품을 삭제합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "상품 삭제 성공"),
-    })
     @DeleteMapping(value = "/{itemId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Response<?>> removeItem(@Parameter(description = "삭제할 상품 ID") @PathVariable Long itemId) {
+    public ResponseEntity<Response<?>> removeItem(@PathVariable Long itemId) {
         itemService.deleteItem(itemId);
 
         log.info("상품 삭제 요청 완료: itemId={}", itemId);
