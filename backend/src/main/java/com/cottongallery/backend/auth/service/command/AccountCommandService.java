82a769067079dbfa23aaf89fedbc986ca3b027df
iju42829 <ijud4282@gmail.com>
2024-11-23 15:50:43 +0900

    docs: Account 서비스 인터페이스 주석 추가

--- backend/src/main/java/com/cottongallery/backend/auth/service/command/AccountCommandService.java
+++ backend/src/main/java/com/cottongallery/backend/auth/service/command/AccountCommandService.java
@@ -1,7 +1,14 @@
 package com.cottongallery.backend.auth.service.command;
 
 import com.cottongallery.backend.auth.dto.account.request.AccountCreateRequest;
+import com.cottongallery.backend.auth.exception.account.UsernameAlreadyExistsException;
 
 public interface AccountCommandService {
+    /**
+     * 사용자 계정을 생성합니다.
+     * @param accountCreateRequest 계정 생성에 필요한 데이터
+     * @return 생성된 계정의 고유 ID
+     * @throws UsernameAlreadyExistsException 사용자명이 이미 사용 중인 경우
+     */
     Long signUp(AccountCreateRequest accountCreateRequest);
 }
