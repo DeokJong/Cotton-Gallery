7e5bd2b4dd2fcf9c8b3f83039170a916e55ad493
oneieo <sun020716@gmail.com>
2024-12-29 00:32:18 +0900

    chore: 마이페이지 page.tsx 추가

---
+++ frontend/src/app/my-page/_components/OrderList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrderList = () => {
+  return <div>주문목록조회</div>;
+};
+
+export default OrderList;
---
+++ frontend/src/app/my-page/edit/password/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>비밀번호 변경</div>;
+};
+
+export default page;
