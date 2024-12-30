9a9a47f104791f706863f6b68440b5b64408abd6
oneieo <sun020716@gmail.com>
2024-12-30 09:30:47 +0900

    chore: 마이페이지 요소 mb 값 조정

--- frontend/src/app/my-page/_components/MyPage.tsx
+++ frontend/src/app/my-page/_components/MyPage.tsx
@@ -16,24 +16,24 @@ const MyPage = () => {
   return (
     <div className="h-screen flex flex-col items-center">
       <h1 className="w-full h-16 border-b-2 text-center border-gray-200 text-[1.75rem] mb-6">마이페이지</h1>
-      <h2 className="w-[36.25rem] h-[3.75rem] border-gray-200 text-[1.75rem]">{name}님</h2>
+      <h2 className="w-[36.25rem] h-[3.75rem] border-gray-200 text-[1.75rem] mt-5 mb-3">{name}님</h2>
       <Link
         href={"/my-page/edit/email"}
-        className="w-[36.25rem] h-[3.75rem] flex justify-between items-center text-[1.25rem] mb-2 p-3 cursor-pointer hover:bg-gray-100 duration-200 rounded-sm"
+        className="w-[36.25rem] h-[3.75rem] flex justify-between items-center text-[1.25rem] mb-4 p-3 cursor-pointer hover:bg-gray-100 duration-200 rounded-sm"
       >
         <button>이메일 변경</button>
         <FaChevronRight color="gray" />
       </Link>
       <Link
         href={"/my-page/edit/password"}
-        className="w-[36.25rem] h-[3.75rem] flex justify-between items-center text-[1.25rem] mb-2 p-3 cursor-pointer hover:bg-gray-100 duration-200 rounded-sm"
+        className="w-[36.25rem] h-[3.75rem] flex justify-between items-center text-[1.25rem] mb-4 p-3 cursor-pointer hover:bg-gray-100 duration-200 rounded-sm"
       >
         <button>비밀번호 변경</button>
         <FaChevronRight color="gray" />
       </Link>
       <Link
         href={"/my-page/order"}
-        className="w-[36.25rem] h-[3.75rem] flex justify-between items-center text-[1.25rem] mb-2 p-3 cursor-pointer hover:bg-gray-100 duration-200 rounded-sm"
+        className="w-[36.25rem] h-[3.75rem] flex justify-between items-center text-[1.25rem] mb-4 p-3 cursor-pointer hover:bg-gray-100 duration-200 rounded-sm"
       >
         <button>주문 내역</button>
         <FaChevronRight color="gray" />
